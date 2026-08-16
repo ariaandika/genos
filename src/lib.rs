6efe98da5e8131e1a9fa143df73068fdd1782590
ariaandika <andikaakun17@gmail.com>
2026-08-16 16:36:03 +0700

    update lints and formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 //! Linux Feature Bindings.
-#![warn(missing_docs)]
-#![allow(clippy::module_inception, clippy::new_without_default)]
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    // for longterm use `unimplemented!()`
+    clippy::todo,
+    clippy::use_debug,
+    clippy::dbg_macro
+)]
+#![allow(clippy::module_inception, clippy::new_without_default, clippy::len_without_is_empty)]
 
 pub mod error;
 pub mod fd;
