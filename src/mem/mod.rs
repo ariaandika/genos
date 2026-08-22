f10c4eebc3d2642c9355d4b6e45cc49045254c13
ariaandika <andikaakun17@gmail.com>
2026-08-22 18:37:37 +0700

    add mem::brk

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -1,5 +1,19 @@
 //! Memory management.
+use core::ptr::NonNull;
+use core::ffi::c_void;
+
 pub use mmap::Mmap;
 
 pub mod alloc;
 pub mod mmap;
+
+/// Sets the end of the data segment to given value.
+///
+/// Returns `None` if failed.
+///
+/// Calling [`brk`] with value of `0` returns the current end of the data segment.
+#[inline]
+pub fn brk(addr: *mut c_void) -> Option<NonNull<c_void>> {
+    let res = crate::sys::call!(RD, __NR_brk, addr).into_inner();
+    usize::try_from(res).ok().and_then(|e| NonNull::new(e as _))
+}
