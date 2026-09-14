9e9deaf481289806b338ccdaddde68b571b20bcb
ariaandika <andikaakun17@gmail.com>
2026-09-14 19:56:33 +0700

    change IoVec{Mut} internal structure

--- src/io/iovec.rs
+++ src/io/iovec.rs
@@ -1,40 +1,39 @@
-//! I/O vector buffer.
-use core::{fmt, marker, mem, slice};
-
-use crate::sys;
+use core::{fmt, marker, mem};
 
 // ===== IoVec =====
 
-/// Vector I/O data structure.
-///
-/// See `iovec(3type)`.
+/// Vector I/O data structure (`iovec(3type)`).
 #[repr(C)]
 pub struct IoVec<'a> {
-    iov: sys::iovec,
+    iov_base: *const u8,
+    iov_len: usize,
     _p: marker::PhantomData<&'a [u8]>,
 }
 
 impl<'a> IoVec<'a> {
+    /// `UIO_FASTIOV`
+    pub const FASTIOV: usize = UIO_FASTIOV;
+
+    /// `UIO_MAXIOV`
+    pub const MAXIOV: usize = UIO_MAXIOV;
+
     /// Creates new [`IoVec`].
     #[inline]
-    pub const fn new(iov: &'a [u8]) -> Self {
-        Self {
-            iov: sys::iovec { iov_base: iov.as_ptr().cast_mut().cast(), iov_len: iov.len() },
-            _p: marker::PhantomData,
-        }
+    pub(crate) const fn new(base: *const u8, len: usize) -> Self {
+        Self { iov_base: base, iov_len: len, _p: marker::PhantomData }
     }
 
-    /// Returns the bytes slice.
+    /// Creates new [`IoVec`] from slice.
     #[inline]
-    pub const fn as_slice(&self) -> &'a [u8] {
-        unsafe { slice::from_raw_parts(self.iov.iov_base.cast(), self.iov.iov_len) }
+    pub const fn from_slice(iov: &'a [u8]) -> Self {
+        Self::new(iov.as_ptr(), iov.len())
     }
 }
 
 impl<'a> From<&'a [u8]> for IoVec<'a> {
     #[inline]
     fn from(value: &'a [u8]) -> Self {
-        Self::new(value)
+        Self::from_slice(value)
     }
 }
 
@@ -46,46 +45,38 @@ impl<'a> fmt::Debug for IoVec<'a> {
 
 // ===== IoVecMut =====
 
-/// Vector I/O data structure.
-///
-/// See `iovec(3type)`.
+/// Vector I/O data structure (`iovec(3type)`).
 #[repr(C)]
 pub struct IoVecMut<'a> {
-    iov: sys::iovec,
+    iov_base: *mut mem::MaybeUninit<u8>,
+    iov_len: usize,
     _p: marker::PhantomData<&'a mut [mem::MaybeUninit<u8>]>,
 }
 
 impl<'a> IoVecMut<'a> {
+    /// `UIO_FASTIOV`
+    pub const FASTIOV: usize = UIO_FASTIOV;
+
+    /// `UIO_MAXIOV`
+    pub const MAXIOV: usize = UIO_MAXIOV;
+
     /// Creates new [`IoVecMut`].
     #[inline]
-    pub const fn new(iov: &'a mut [u8]) -> Self {
-        Self {
-            iov: sys::iovec { iov_base: iov.as_mut_ptr().cast(), iov_len: iov.len() },
-            _p: marker::PhantomData,
-        }
+    pub(crate) const fn new(base: *mut mem::MaybeUninit<u8>, len: usize) -> Self {
+        Self { iov_base: base, iov_len: len, _p: marker::PhantomData }
     }
 
-    /// Creates new [`IoVecMut`] from uninitialized bytes.
+    /// Creates new [`IoVecMut`] from slice.
     #[inline]
-    pub const fn from_uninit(iov: &'a mut [mem::MaybeUninit<u8>]) -> Self {
-        Self {
-            iov: sys::iovec { iov_base: iov.as_mut_ptr().cast(), iov_len: iov.len() },
-            _p: marker::PhantomData,
-        }
-    }
-}
-
-impl<'a> From<&'a mut [u8]> for IoVecMut<'a> {
-    #[inline]
-    fn from(value: &'a mut [u8]) -> Self {
-        Self::new(value)
+    pub const fn from_slice(iov: &'a mut [mem::MaybeUninit<u8>]) -> Self {
+        Self::new(iov.as_mut_ptr(), iov.len())
     }
 }
 
 impl<'a> From<&'a mut [mem::MaybeUninit<u8>]> for IoVecMut<'a> {
     #[inline]
     fn from(value: &'a mut [mem::MaybeUninit<u8>]) -> Self {
-        Self::from_uninit(value)
+        Self::from_slice(value)
     }
 }
 
@@ -94,3 +85,10 @@ impl<'a> fmt::Debug for IoVecMut<'a> {
         f.debug_tuple("IoVecMut").finish_non_exhaustive()
     }
 }
+
+// ===== extern =====
+
+// include/uapi/linux/uio.h
+
+const UIO_FASTIOV: usize = 8;
+const UIO_MAXIOV: usize = 1024;
