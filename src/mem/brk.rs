b7468c2b72f209536b484c998b251b1aaf4303ad
ariaandika <andikaakun17@gmail.com>
2026-09-25 11:34:58 +0700

    fix: brk signature

--- src/mem/brk.rs
+++ src/mem/brk.rs
@@ -1,13 +1,16 @@
 use core::ffi::c_void;
+use core::mem;
 use core::ptr::NonNull;
 
-use crate::sys::{self, Error, arch};
+use crate::sys;
 
 /// Sets the end of the data segment to given value (`brk(2)`).
 ///
 /// Note that this is pure system calls to `brk`, any explanation in `brk(2)` may refer to glibc
 /// implementation.
 #[inline]
-pub fn brk(addr: *mut c_void) -> Result<NonNull<c_void>, Error<arch::sys_brk>> {
-    sys::call_rd!(sys_brk, addr)
+pub fn brk(addr: *mut c_void) -> NonNull<c_void> {
+    let res = sys::call_rd_raw!(sys_brk, addr);
+    // SAFETY: `sys_brk` never returns zero
+    unsafe { mem::transmute(res) }
 }
